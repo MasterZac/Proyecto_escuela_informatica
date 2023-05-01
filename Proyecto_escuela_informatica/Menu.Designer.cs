52d633861da346d9093ef6259a62e4e14c5e81a1
ZACARIAS VENTURA MARTINEZ <L205Q0033@cosamaloapan.tecnm.mx>
2023-04-30 23:50:14 -0600

    Avance proyecto

--- Proyecto_escuela_informatica/Menu.Designer.cs
+++ Proyecto_escuela_informatica/Menu.Designer.cs
@@ -28,93 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnProfesor = new System.Windows.Forms.Button();
+            this.BtnAlumno = new System.Windows.Forms.Button();
+            this.BtnGrupo = new System.Windows.Forms.Button();
+            this.BtnTFC = new System.Windows.Forms.Button();
+            this.BtnTribunal = new System.Windows.Forms.Button();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(80, 76);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 100);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Alumno";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.panel1.Controls.Add(this.BtnSalir);
+            this.panel1.Controls.Add(this.BtnTribunal);
+            this.panel1.Controls.Add(this.BtnTFC);
+            this.panel1.Controls.Add(this.BtnGrupo);
+            this.panel1.Controls.Add(this.BtnAlumno);
+            this.panel1.Controls.Add(this.BtnProfesor);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 639);
+            this.panel1.TabIndex = 0;
             // 
-            // button2
+            // BtnProfesor
             // 
-            this.button2.Location = new System.Drawing.Point(310, 76);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 100);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Grupo";
-            this.button2.UseVisualStyleBackColor = true;
+            this.BtnProfesor.FlatAppearance.BorderSize = 0;
+            this.BtnProfesor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnProfesor.ForeColor = System.Drawing.Color.White;
+            this.BtnProfesor.Location = new System.Drawing.Point(0, 27);
+            this.BtnProfesor.Name = "BtnProfesor";
+            this.BtnProfesor.Size = new System.Drawing.Size(281, 83);
+            this.BtnProfesor.TabIndex = 1;
+            this.BtnProfesor.Text = "PROFESOR";
+            this.BtnProfesor.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // BtnAlumno
             // 
-            this.button3.Location = new System.Drawing.Point(554, 76);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(148, 100);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Profesor ";
-            this.button3.UseVisualStyleBackColor = true;
+            this.BtnAlumno.FlatAppearance.BorderSize = 0;
+            this.BtnAlumno.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAlumno.ForeColor = System.Drawing.Color.White;
+            this.BtnAlumno.Location = new System.Drawing.Point(3, 116);
+            this.BtnAlumno.Name = "BtnAlumno";
+            this.BtnAlumno.Size = new System.Drawing.Size(281, 83);
+            this.BtnAlumno.TabIndex = 2;
+            this.BtnAlumno.Text = "ALUMNO";
+            this.BtnAlumno.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // BtnGrupo
             // 
-            this.button4.Location = new System.Drawing.Point(80, 243);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(148, 100);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "TFC";
-            this.button4.UseVisualStyleBackColor = true;
+            this.BtnGrupo.FlatAppearance.BorderSize = 0;
+            this.BtnGrupo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGrupo.ForeColor = System.Drawing.Color.White;
+            this.BtnGrupo.Location = new System.Drawing.Point(0, 205);
+            this.BtnGrupo.Name = "BtnGrupo";
+            this.BtnGrupo.Size = new System.Drawing.Size(281, 83);
+            this.BtnGrupo.TabIndex = 3;
+            this.BtnGrupo.Text = "GRUPO";
+            this.BtnGrupo.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // BtnTFC
             // 
-            this.button5.Location = new System.Drawing.Point(320, 243);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(148, 100);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Tribunal";
-            this.button5.UseVisualStyleBackColor = true;
+            this.BtnTFC.FlatAppearance.BorderSize = 0;
+            this.BtnTFC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnTFC.ForeColor = System.Drawing.Color.White;
+            this.BtnTFC.Location = new System.Drawing.Point(3, 294);
+            this.BtnTFC.Name = "BtnTFC";
+            this.BtnTFC.Size = new System.Drawing.Size(281, 83);
+            this.BtnTFC.TabIndex = 4;
+            this.BtnTFC.Text = "TFC";
+            this.BtnTFC.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // BtnTribunal
             // 
-            this.button6.Location = new System.Drawing.Point(554, 243);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(148, 100);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.BtnTribunal.FlatAppearance.BorderSize = 0;
+            this.BtnTribunal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnTribunal.ForeColor = System.Drawing.Color.White;
+            this.BtnTribunal.Location = new System.Drawing.Point(3, 383);
+            this.BtnTribunal.Name = "BtnTribunal";
+            this.BtnTribunal.Size = new System.Drawing.Size(281, 83);
+            this.BtnTribunal.TabIndex = 5;
+            this.BtnTribunal.Text = "TRIBUNAL";
+            this.BtnTribunal.UseVisualStyleBackColor = true;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.FlatAppearance.BorderSize = 0;
+            this.BtnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSalir.ForeColor = System.Drawing.Color.White;
+            this.BtnSalir.Location = new System.Drawing.Point(0, 556);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(281, 83);
+            this.BtnSalir.TabIndex = 6;
+            this.BtnSalir.Text = "SALIR";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(281, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(863, 36);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Proyecto_escuela_informatica.Properties.Resources.LOGO_VERTICAL_TECNM;
+            this.pictureBox1.Location = new System.Drawing.Point(906, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(306, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(688, 286);
+            this.label1.TabIndex = 3;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 480);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1144, 639);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
             this.Load += new System.EventHandler(this.Menu_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnProfesor;
+        private System.Windows.Forms.Button BtnTribunal;
+        private System.Windows.Forms.Button BtnTFC;
+        private System.Windows.Forms.Button BtnGrupo;
+        private System.Windows.Forms.Button BtnAlumno;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
